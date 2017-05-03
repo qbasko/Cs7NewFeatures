9a75c97d182c6d60bdfa979841cb5ccd00857890
qbasko <qbasko@gmail.com>
2017-05-03 19:25:27 +0200

    added project with out variables sample

---
+++ Cs7NewFeatures/OutVariables.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cs7NewFeatures
+{
+    public class OutVariables
+    {
+        public void Cs6Style()
+        {
+            int a, b;
+            TestMethod(out a, out b);
+            Console.WriteLine($"a = {a} \t b = {b} ");
+        }
+
+        public void Cs7Style()
+        {
+            TestMethod(out int a, out int b);
+            TestMethod(out var c, out var d);
+            TestMethod(out _, out _); //I don't care about out variables
+
+            Console.WriteLine($"a = {a} \t b = {b} ");
+            Console.WriteLine($"c = {c} \t d = {d} ");
+        }
+
+
+        public void TestMethod(out int a, out int b)
+        {
+            a = 11;
+            b = 22;
+        }
+    }
+}
---
+++ Cs7NewFeatures/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cs7NewFeatures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TestMethod(out int a, out int b);
+            Console.WriteLine($"a = {a} \t b = {b} ");
+        }
+
+
+        static void TestMethod(out int a, out int b)
+        {
+            a = 1;
+            b = 1;
+        }
+    }
+}
